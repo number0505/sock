cf320fb8ccc3181a61eff661df5f89bc0239ff77
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-02 16:41:19 -0400

    update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,13 @@
-const videoElement = document.getElementById('videoElement');
+document.getElementById('startCamera').addEventListener('click', function() {
+	const videoElement = document.getElementById('videoElement');
 
-if (navigator.mediaDevices.getUserMedia) {
-		navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-		.then(function (stream) {
-				videoElement.srcObject = stream;
-		})
-		.catch(function (err0r) {
-				console.log("카메라에 접근할 수 없습니다.");
-		});
+	if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+			.then(function (stream) {
+					videoElement.srcObject = stream;
+			})
+			.catch(function (error) {
+					console.log("Cannot access camera.",error);
+			});
+	}
 }
