005d8d0d35c59a9a8aac59776ccee9de37f2c466
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-13 15:13:02 -0400

    embed imgs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
   const scrollTop = window.pageYOffset;
-  const page1 = document.querySelector('.page1');
-  const page2 = document.querySelector('.page2');
+  const page1 = document.querySelector('.sock_img');
+  const page2 = document.querySelector('.title');
 
   page1.style.transform = `translate(-50%, -50%) scale(${1 - scrollTop / 100})`;
   page2.style.transform = `translate(-50%, -50%) scale(${scrollTop / 100 + 1})`;
