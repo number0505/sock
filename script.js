4a80d07fbe467dd7c82dbf4c9dcb5c837725948d
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-13 15:23:43 -0400

    intersection of images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
   const scrollTop = window.pageYOffset;
-  const page1 = document.querySelector('.sock_img');
-  const page2 = document.querySelector('.title');
+  const page1 = document.querySelector('.title');
+  const page2 = document.querySelector('.sock_bg');
 
   page1.style.transform = `translate(-50%, -50%) scale(${1 - scrollTop / 100})`;
   page2.style.transform = `translate(-50%, -50%) scale(${scrollTop / 100 + 1})`;
