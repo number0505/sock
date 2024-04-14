8ad54c5ce2dbc4f11e904ef2da39717928cb4708
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-14 13:53:55 -0400

    edit sock_bg.png

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ window.addEventListener('scroll', () => {
 
 	const percentage = scrollPosition / maxScroll;
 	// max(1... 은 축소했을 때 최대1 보다 작아지지 않도록 / 처음 200에서 100까지 축소
-	const scaleDown = Math.max(1, 15 - 15 * percentage);	// min(1... 은 확대했을 때 최소 1 보다 작아지지 않도록 / 처음 50에서 100까지 확대??
-	const scaleUp = Math.min(1.2, 2 - 2 * percentage);
+	const scaleDown = Math.max(1, 15 - 14 * percentage);	// min(1... 은 확대했을 때 최소 1 보다 작아지지 않도록 / 처음 50에서 100까지 확대??
+	const scaleUp = Math.min(2, 1 + 1 * percentage);
 
 	document.getElementById('page1').style.transform = `scale(${scaleDown})`;
 	document.getElementById('page2').style.transform = `scale(${scaleUp})`;
