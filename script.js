a0412df46b7c85f67e8dcf1e230b481d6f14da76
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-14 07:28:41 -0400

    Scroll min/max

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// window.addEventListener('scroll', function() {
-//   const scrollTop = window.pageYOffset;
-//   const page1 = document.querySelector('.title');
-//   const page2 = document.querySelector('.sock_bg');
 
-//   page1.style.transform = `translate(-50%, -50%) scale(${1 - scrollTop / 100})`;
-//   page2.style.transform = `translate(-50%, -50%) scale(${scrollTop / 100 + 1})`;
-// });
 
 window.addEventListener('scroll', () => {
 	const scrollPosition = window.pageYOffset;
 	const maxScroll = window.innerHeight;
 
-	const scaleUp = Math.min(1, 0.5 + scrollPosition / maxScroll * 0.5);
-	const scaleDown = Math.max(0.5, 1 - scrollPosition / maxScroll * 0.5);
+	if(scrollPosition >= maxScroll) {
+		window.scrollTo(0. maxScroll);
+	}
 
-	document.getElementById('page2').style.transform = `scale(${scaleUp})`;
-	document.getElementById('page1').style.transform = `scale(${scaleDown})`;
+	const percentage = scrollPosition / maxScroll;
+	// max(1... 은 축소했을 때 최대1 보다 작아지지 않도록 / 처음 200에서 100까지 축소
+	const scaleDown = Math.max(1, 2 - 1 * percentage); 
+	// min(1... 은 확대했을 때 최소 1 보다 작아지지 않도록 / 처음 50에서 100까지 확대
+	const scaleUp = Math.min(1, 0.5 - 0.5 * percentatge);
+	
+
+
+
+    document.getElementById('page2').style.transform = `scale(${scaleUp})`;
+    document.getElementById('page1').style.transform = `scale(${scaleDown})`;
 });
+
