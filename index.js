6573cdcdf18a04875db2290bfef36685e2d69de6
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-15 16:33:54 -0400

    install packages

--- index.js
+++ index.js
@@ -4,14 +4,18 @@
 
 //https://www.npmjs.com/package/@google-cloud/vision
 
-//
 
-const express = require('express');
-const fs = require('fs');
-const util = require('util');
-const mime = require('mime');
-const multer = require('multer');
+var express = require('express');
+var fs = require('fs');
+var util = require('util');
+var mime = require('mime');
+var multer = require('multer');
 
 var upload = multer({dest: './uploaded'})
 
 var sockDB = require('./resources/Sock DB.json');
+
+//https://cloud.google.com/vision/docs/detecting-properties#vision_image_property_detection-nodejs
+const vision = require('@google-cloud/vision');
+
+var app = express();
