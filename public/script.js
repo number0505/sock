f8f940f31214e6dccd5b088c62f26ce47008af8d
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-22 16:32:30 -0400

    desktop breakpoint

--- public/script.js
+++ public/script.js
@@ -3,17 +3,23 @@
 window.addEventListener('scroll', () => {
 	const scrollPosition = window.pageYOffset;
 	const maxScroll = window.innerHeight;
+	let screenSizeModifer;
+	if (window.innerWidth <= 768) {
+		screenSizeModifer = '_mobile'
+	} else {
+		screenSizeModifer = '_desktop'
+	}
+	const nextPageButton = document.getElementById('nextPageButton' + screenSizeModifer)
 
 	if(scrollPosition >= maxScroll) {
 		window.scrollTo(0, maxScroll);
-	
-		if (document.getElementById('nextPageButton')) {
-			document.getElementById('nextPageButton').style.display = 'block'; // 버튼 표시
+		if (nextPageButton) {
+			nextPageButton.style.display = 'block'; // 버튼 표시
 			nextPageButton.classList.add('active');
 		}
 	} else {
-		if (document.getElementById('nextPageButton')) {
-			document.getElementById('nextPageButton').style.display = 'none'; // 스크롤 중 버튼 숨김
+		if (nextPageButton) {
+			nextPageButton.style.display = 'none'; // 스크롤 중 버튼 숨김
 			nextPageButton.classList.remove('active'); 
 		}
 	}
@@ -24,22 +30,27 @@ window.addEventListener('scroll', () => {
 	// min(1... 은 확대했을 때 최소 1 보다 작아지지 않도록 / 처음 50에서 100까지 확대??
 	const scaleUp = Math.min(2, 1 + 1 * percentage);
 
-	if (document.getElementById('page1')) {
-		document.getElementById('page1').style.transform = `scale(${scaleDown})`;
+	if (document.getElementById('page1' + screenSizeModifer)) {
+		document.getElementById('page1' + screenSizeModifer).style.transform = `scale(${scaleDown})`;
 	}
 	
-	if (document.getElementById('page2')) {
-		document.getElementById('page2').style.transform = `scale(${scaleUp})`;
+	if (document.getElementById('page2' + screenSizeModifer)) {
+		document.getElementById('page2' + screenSizeModifer).style.transform = `scale(${scaleUp})`;
 	}
 });
 
 
-document.getElementById('nextPageButton')?.addEventListener('click', () => {
+document.getElementById('nextPageButton_desktop')?.addEventListener('click', () => {
 	// 다음 페이지로 이동하는 로직 추가 (예: 새로운 URL로 리다이렉션)
 	window.location.href = '/upload'; //원하는 페이지로 변경
 });
-document.querySelector('.custom-file-input').addEventListener('click', function() {
-    document.getElementById('fileInput').click();
+
+document.getElementById('nextPageButton_mobile')?.addEventListener('click', () => {
+	// 다음 페이지로 이동하는 로직 추가 (예: 새로운 URL로 리다이렉션)
+	window.location.href = '/upload'; //원하는 페이지로 변경
 });
+// document.querySelector('.custom-file-input').addEventListener('click', function() {
+//     document.getElementById('fileInput').click();
+// });
 
 
