9ab00a9b70ff30f153903ee64a24664162b661d8
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-22 12:38:16 -0400

    design description section

--- index.js
+++ index.js
@@ -62,24 +62,26 @@ app.post('/result', upload.single('image'), async function(req, res, next) {
     const rgb = `rgb(${sockFound.r} ${sockFound.g} ${sockFound.b})`;
     res.write(`
       <h1>The traits of your sock</h1>
-      <svg width="186" height="302" viewBox="0 0 208 320" xmlns="http://www.w3.org/2000/svg">
-      <path d="M117.857 0.0620117L90.6561 151.03L80.781 170.146L8.89683 246.326L0.756836 270.803L10.9759 297.038L34.578 301.967L59.4596 292.968L131.988 236.955L151.867 224.525L162.937 209.3L165.541 183.017L159.557 157.04L185.244 11.9001L117.857 0.0620117Z" fill="${rgb}" stroke="#4142F4" stroke-width="5"/>
-      </svg>
-      <p>
-        <span class="rgb r">R</span>
-        <span>${sockFound.r}</span>
-        <span class="rgb g">G</span>
-        <span>${sockFound.g}</span>
-        <span class="rgb b">B</span>
-        <span>${sockFound.b}</span>
-      </p>
-        <span class="tag"> ${sockFound.tag1}</span>
-        <span class="tag"> ${sockFound.tag2}</span> 
-        <span class="tag"> ${sockFound.tag3}</span>
-      </p>
-      <img width=200 src="resources/${sockFound.file}"></img>
-      <p>${sockFound.description}</p>
-
+      <main>
+        <svg width="186" height="302" viewBox="0 0 208 320" xmlns="http://www.w3.org/2000/svg">
+          <path d="M117.857 0.0620117L90.6561 151.03L80.781 170.146L8.89683 246.326L0.756836 270.803L10.9759 297.038L34.578 301.967L59.4596 292.968L131.988 236.955L151.867 224.525L162.937 209.3L165.541 183.017L159.557 157.04L185.244 11.9001L117.857 0.0620117Z" fill="${rgb}" stroke="#4142F4" stroke-width="5"/>
+        </svg>
+        <p>
+          <span class="rgb r">R</span>
+          <span>${sockFound.r}</span>
+          <span class="rgb g">G</span>
+          <span>${sockFound.g}</span>
+          <span class="rgb b">B</span>
+          <span>${sockFound.b}</span>
+        </p>
+        <p>
+          <span class="tag"> ${sockFound.tag1}</span>
+          <span class="tag"> ${sockFound.tag2}</span> 
+          <span class="tag"> ${sockFound.tag3}</span>
+        </p>
+        <img width=200 src="resources/${sockFound.file}"></img>
+        <p class="description">${sockFound.description}</p>
+      </main>
     `)
   } else {
     res.write(`<p>No sock found</p>`);
