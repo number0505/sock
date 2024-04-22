2d097a551b736e63ae25a9fd765233194014d602
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-22 09:59:01 -0400

    add font link

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/result', upload.single('image'), async function(req, res, next) {
 
 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" type="text/css" href="/assets/result.css"><link rel="stylesheet" type="text/css" href="/assets/style.css"></head><body>`);
+  res.write(`<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" type="text/css" href="/assets/result.css"><link rel="stylesheet" type="text/css" href="/assets/style.css"><link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet"></head><body>`);
 
   res.write(`<a href='/upload' class='btn'>Back</a>`);
   // res.write(`<img src="${imageUrl}" width="200"></img>`); //업로드한 이미지
