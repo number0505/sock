a1a5861a19287ab39cad1b8039e8e8a1a358c44c
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-23 16:19:25 -0400

    set div

--- index.js
+++ index.js
@@ -64,9 +64,12 @@ app.post('/result', upload.single('image'), async function(req, res, next) {
 
       <main>
         <h1>Your sock’s profile</h1>
-        <svg width="186" height="302" viewBox="0 0 208 320" xmlns="http://www.w3.org/2000/svg">
+        <div >
+          <svg width="186" height="302" viewBox="0 0 208 320" xmlns="http://www.w3.org/2000/svg">
           <path d="M117.857 0.0620117L90.6561 151.03L80.781 170.146L8.89683 246.326L0.756836 270.803L10.9759 297.038L34.578 301.967L59.4596 292.968L131.988 236.955L151.867 224.525L162.937 209.3L165.541 183.017L159.557 157.04L185.244 11.9001L117.857 0.0620117Z" fill="${rgb}" stroke="#4142F4" stroke-width="5"/>
-        </svg>
+          </svg>
+        </div>
+        
         <p>
           <span class="rgb r">R</span>
           <span>${sockFound.r}</span>
