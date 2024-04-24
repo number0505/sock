b9cfd33670c2b749c86208cf0ed548da80ff0638
number0505 <143631112+number0505@users.noreply.github.com>
2024-04-23 22:39:10 -0400

    edit gradient

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/result', upload.single('image'), async function(req, res, next) {
       <main>
         <section>
           <img class="icon_img" src="content/icon_analysis.svg">
-          <h1>Your sock’s profile</h1>
+          <h1 class="section_title">Your sock’s profile</h1>
           <div class="sock_illustration whitebox">
             <svg width="186" height="302" viewBox="0 0 208 320" xmlns="http://www.w3.org/2000/svg">
             <path d="M117.857 0.0620117L90.6561 151.03L80.781 170.146L8.89683 246.326L0.756836 270.803L10.9759 297.038L34.578 301.967L59.4596 292.968L131.988 236.955L151.867 224.525L162.937 209.3L165.541 183.017L159.557 157.04L185.244 11.9001L117.857 0.0620117Z" fill="${rgb}" stroke="#4142F4" stroke-width="5"/>
@@ -101,7 +101,7 @@ app.post('/result', upload.single('image'), async function(req, res, next) {
 
         <section>
           <img class="icon_img" src="content/icon_matching.svg">
-          <h1>The perfect match for your sock</h1>
+          <h1 class="section_title">The perfect match for your sock</h1>
           <img class="whitebox" width=200 src="resources/${sockFound.file}"></img>
           <p class="description whitebox">${sockFound.description}</p>
           <div class="btn">
